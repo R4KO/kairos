2651f12472440b529f138f8951c9fe253f91c8e9
R4KO <rakotovahiny@hotmai.fr>
2020-05-07 04:11:43 +0200

    Les boutons gardent leurs couleurs

--- script/inner.js
+++ script/inner.js
@@ -1,6 +1,20 @@
-function innertest() {
+const buttons = document.querySelectorAll('.button');
 
-    var elem = document.getElementById('test');
-    elem.innerHTML = "test réussi";
-
+const colors = {
+	caresse: '#3acf6e',
+	déman: '#3a73cf',
+	fix: '#c5cf3a'
 }
+
+
+document.addEventListener("DOMContentLoaded", function addclick() {
+	buttons.forEach(function(target) {
+		target.onclick = function(t) {
+			// rendre les boutons blancs
+			buttons.forEach(function(target) {
+				target.style.background = 'white';
+			});
+			t.target.style.background = colors[t.target.id];
+		}
+	});
+});
