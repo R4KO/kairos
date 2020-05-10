6cdcbcf565bcb6ec7a5030d416008ee766ba78cd
LECUYER Maxime <maxime.lecuyer@efrei.net>
2020-05-10 12:15:16 +0200

    changement ID boutons + alert on click sur un bouton pour des tests

--- script/inner.js
+++ script/inner.js
@@ -1,20 +1,20 @@
 const buttons = document.querySelectorAll('.button');
 
 const colors = {
-	caresse: '#3acf6e',
-	déman: '#3a73cf',
-	fix: '#c5cf3a'
+    C: '#3acf6e',
+    D: '#3a73cf',
+    F: '#c5cf3a'
 }
 
 
 document.addEventListener("DOMContentLoaded", function addclick() {
-	buttons.forEach(function(target) {
-		target.onclick = function(t) {
-			// rendre les boutons blancs
-			buttons.forEach(function(target) {
-				target.style.background = 'white';
-			});
-			t.target.style.background = colors[t.target.id];
-		}
-	});
+    buttons.forEach(function(target) {
+        target.onclick = function(t) {
+            // rendre les boutons blancs
+            buttons.forEach(function(target) {
+                target.style.background = 'white';
+            });
+            t.target.style.background = colors[t.target.id];
+        }
+    });
 });
