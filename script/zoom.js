88fa9bf8e7fc36f3fd6bf1e8471026dd42c04cb8
R4KO <rakotovahiny@hotmai.fr>
2020-05-20 05:12:59 +0200

    modèle pour les zones de prêt

--- script/zoom.js
+++ script/zoom.js
@@ -1,26 +1,10 @@
+var path = "";
 const chemin = "./img/body/";
 const bodySection = document.getElementsByTagName('svg');
-const humanBody = document.querySelector('#avatar');
-const imageSection = document.querySelector('#img');
-const buttons = document.querySelectorAll('.button');
-
-
-function Values(){
-    this.action=null ;
-    this.body1=null;
-
-}
-/**
- Values.prototype.Setaction = function (Action) {
-   this.action = Action;
-};
-
- Values.prototype.getaction = function () {
-    return this.action;
-};**/
-var valeurs =  new Values();
-
-const colors = {
+const humanBody = document.querySelector('#avatar'); // avatar svg
+const imageSection = document.querySelector('#img'); // section vide qui contiendra les images après le click
+const buttons = document.querySelectorAll('.button'); // type d'autocontact
+const colors = { // pour les boutons
     C: '#3acf6e',
     D: '#3a73cf',
     F: '#c5cf3a'
@@ -34,9 +18,7 @@ const definitions = {
     }
 }
 
-var path = "";
-
-var bodyParts = {
+const bodyParts = {
     head: [
         "tete/head_face.png",
         //"tete/head_back.png",
@@ -77,83 +59,101 @@ var bodyParts = {
     ]
 };
 
+// les sous-objets représentent le nom du dossier
 const zones = {
+    tete: {
+
+    },
+
+    epaule: {
+
+    },
+
+    torse: {
+        "2_P10": [54, 81],
+    },
+
+    bras: {
+
+    },
+
+    ventre: {
+
+    },
+
+    mains: {
+
+    },
+
     jambes: {
         72: [31, 14],
         73: [32, 39],
-    },
-};
-
-/*
-var pointless = {
-    head: [
-        "tete/head_face.png",
-        //"tete/head_back.png",
-        //"tete/head_right.png"
-    ],
-
-    // attention au cou
-
-    shoulder: [
-        "epaule/epaule_face.png"
-    ],
-
-    cheast: [
-        "torse/torse_face.png",
-        //"torse/torse_back.png"
-    ],
-
-    arm: [
-        "bras/bras_face.png",
-        //"bras/bras_back.png"
-    ],
-
-    hand: [
-        "mains/mains_face.png"
-    ],
-
-    // attention au bassin
-
-    stomach: [
-        "ventre/ventre_face.png",
-        //"ventre/ventre_back.png"
-    ],
-
-    legs: [
-        "jambes/jambes_face.png",
-        //"jambes/jambes_back.png",
-        //"jambes/jambes_right.png"
-    ]
-};
-
-var fixation = {
-
-};
-
-var caresse = {
-
-};
-
-var demangeaison = {
-
+    }
 };
 
 
-var bodyParts = pointless;
+function Values(){
+    this.action=null ;
+    this.body1=null;
 
-*/
+}
+
+/**
+ Values.prototype.Setaction = function (Action) {
+   this.action = Action;
+};
+
+ Values.prototype.getaction = function () {
+    return this.action;
+};**/
+
+var valeurs =  new Values();
+
+/** Fonctions primordiales */
 
 document.addEventListener("DOMContentLoaded", init);
+
+document.addEventListener("DOMContentLoaded", function addclick() {
+    let button;
+
+    // on récupère les boutons relatifs au type d'autocontact
+    buttons.forEach(function(target) {
+        target.onclick = function(t) {
+
+            // rendre tous les boutons blancs
+            buttons.forEach(function(target) {
+                target.style.background = 'white';
+            });
+
+            let id = t.target.id;
+            let mouvement = definitions.mouvement[id];
+
+            // attribuer la bonne couleur de fond
+            t.target.style.background = colors[id];
+            
+            // Pour changer le path des images il faut altérer le path
+            path = chemin + mouvement + "/";
+
+            button = t.target.id;
+            if (button === 'C' || button === 'D' || button === 'F'){
+                valeurs.action = button;
+                console.log(valeurs.action) ; // --> ça marche
+            }else{
+                getButtonValue();
+            }
+        }
+    });
+});
 
 function init() {
     Array.from(bodySection).forEach(addClickAttribute);
 }
 
 function addClickAttribute(part, index) {
-    //console.log(part);
-
+    // part représente une section svg du corps humain
     part.onclick = function(t) {
         if (path === "") {
+            // aucun bouton n'a été cliqué
             window.alert("Sélectionnez le type d'autocontact");
         }
 
@@ -164,13 +164,14 @@ function addClickAttribute(part, index) {
 
             // récupérer la section cliquée
             let element = t.target.getAttribute('data-position');
+
             if (element == null) {
                 // au cas où on clique au mauvais endroit
                 element = t.target.parentElement.getAttribute('data-position');
             }
 
-            //console.log(element);
             valeurs.body1= converteur(element);
+
             // chercher les paths des images correspondantes
             if (bodyParts[element]) {
                 bodyParts[element].forEach(insertImage);
@@ -181,13 +182,18 @@ function addClickAttribute(part, index) {
 }
 
 function insertImage(part, index) {
+    // part représente le path contenu dans l'object bodyParts
+
     // créée l'image
     var image = document.createElement('img');
     image.src = path + part;
     image.className = "bodyImage";
     image.id = part.match(/.*\//)[0].replace("/", ""); // pour récupérer le nom de la partie du corps
+
+    // Pour récupérer les coordonnées
     image.addEventListener('click', printPosition);
 
+    // placer l'image dans la div #img
     imageSection.appendChild(image);
 }
 
@@ -197,17 +203,31 @@ function printPosition(t) {
     position.innerHTML = getPosition(t);
 }
 
-
 function getPosition(e) {
     var posXR = getMousePositionX(e);
     var posYR = getMousePositionY(e);
 
-    //console.log(getZoneFrom(posXR, posYR));
     // pour récupérer la bonne partie du corps
     getZoneFrom(zones[e.target.id] , posXR, posYR);
 
     return 'Image ' + e.target.id + '<br />Position X: ' + posXR + ' %<br />Position Y: ' + posYR + ' %';
 }
+
+function getZoneFrom(zone, x, y) {
+    var marge = 5; // en pourcentage de l'image
+
+    // On retourne l'objet en récupérant que les tableaux de coordonnées
+    Object.values(zone).some((arr) => {
+        // on vérifie que les coordonnées du click correspondent à une zone définie dans l'objet zone
+        if (checkMarge(x, arr[0], marge) && checkMarge(y, arr[1], marge)) {
+            // la zone existe donc on récupère sa key dans l'objet
+            console.log(getKeyByValue(zone, arr));
+        }
+    });
+}
+
+
+/** Fonctions utilitaires */
 
 function getMousePositionX(event) {
     var rect = event.target.getBoundingClientRect();
@@ -234,56 +254,16 @@ function getMousePositionY(event) {
     return Math.round((y / height) * 100 * 100) / 100;
 }
 
-function getZoneFrom(zone, x, y) {
-    Object.values(zone).some((arr) => {
-        //console.log(arr);
-
-        // 5% de marge d'erreur
-        if (checkMarge(x, arr[0], 5) && checkMarge(y, arr[1], 5)) {
-
-            // get zone number
-            console.log(getKeyByValue(zone, arr));
-        }
-    });
-}
-
 function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
+    // ce qui est important c'est que ça fonctionne
+    return Object.keys(object).find(key => object[key] === value);
 }
 
-// marge représente la marge d'erreur en %
+// marge représente la marge d'erreur admise
 function checkMarge(number, reference, marge) {
+    // on créée un intervalle
     return (number > (reference - marge)) && (number < (reference + marge))
 }
-
-document.addEventListener("DOMContentLoaded", function addclick() {
-    let button;
-    buttons.forEach(function(target) {
-        target.onclick = function(t) {
-            // rendre les boutons blancs
-            buttons.forEach(function(target) {
-                target.style.background = 'white';
-            });
-
-            let id = t.target.id;
-            let mouvement = definitions.mouvement[id];
-
-            t.target.style.background = colors[id];
-            
-            // Pour changer le path des images
-            path = chemin + mouvement + "/";
-            button = t.target.id;
-            if (button === 'C' || button === 'D' || button === 'F'){
-                valeurs.action = button;
-                console.log(valeurs.action) ; // --> ça marche
-            }else{
-                getButtonValue();
-            }
-        }
-    });
-});
-
-
 
 function getButtonValue() {
     window.location.assign("test.php?action="+valeurs.action+"&&test="+valeurs.body1);
